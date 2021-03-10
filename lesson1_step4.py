bbff720bd90a1be370c20ca7dc9af436c1dbfabb
Martinov Roman <geliy183@gmail.com>
2021-03-10 21:08:20 +0300

    Add py code for one of the lessons via bash

---
+++ lesson1_step4.py
@@ -0,0 +1,31 @@
+import time
+import math
+from selenium import webdriver
+
+
+def calc(x):
+    return str(math.log(abs(12*math.sin(int(x)))))
+
+
+try:
+    driver = webdriver.Chrome()
+
+    driver.get('http://suninjuly.github.io/math.html')
+    time.sleep(5)
+
+    x_element = driver.find_element_by_id("input_value")
+    x_element = x_element.text
+    answer_field = driver.find_element_by_id("answer")
+    answer_field.send_keys(calc(int(x_element)))
+
+    rob_check = driver.find_element_by_tag_name("[type='checkbox']")
+    rob_check.click()
+
+    rob_cool_check = driver.find_element_by_id("robotsRule")
+    rob_cool_check.click()
+
+    submit_button = driver.find_element_by_tag_name('[type="submit"]')
+    submit_button.click()
+finally:
+    time.sleep(20)
+    driver.quit()
